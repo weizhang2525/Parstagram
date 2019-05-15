b48fab418835e8b59de613d70da9ee6abf8855f2
Wei Zhang <wjzhang1@uci.edu>
2019-05-14 21:47:34 -0700

    first part of parstagram completed

---
+++ Parstagram/FeedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FeedViewController.swift
+//  Parstagram
+//
+//  Created by Wei Zhang on 5/14/19.
+//  Copyright © 2019 Wei Zhang. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
